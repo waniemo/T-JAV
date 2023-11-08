285d65989314d3edafd37a8fb74498abb6686535
waniemo <evaducere@gmail.com>
2023-11-08 15:22:38 +0100

    FEAT: Added class Hericendre, Zekrom, Rondoudou

---
+++ src/Hericendre.java
@@ -0,0 +1,11 @@
+public class Hericendre extends Pokemon{
+
+    public Hericendre() {
+        super("Héricendre", Feu.class, 1, 39, 52, 43, 65, "hericendre.png", "hericendre_f.gif", "hericendre_b.gif");
+    }
+
+    @Override
+    public void attack() {
+
+    }
+}
---
+++ src/Rondoudou.java
@@ -0,0 +1,9 @@
+public class Rondoudou extends Pokemon{
+    public Rondoudou() {
+        super("Rondoudou", Fee.class, 1, 115, 45, 20, 20, "rondoudou.png", "rondoudou_f.gif", "rondoudou_b.gif");
+    }
+
+    @Override
+    public void attack() {
+    }
+}
