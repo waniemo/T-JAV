917d3fc55a9cd2a8d969e91be144b71c853179a1
mminguez <mathieu.minguez@gmail.com>
2023-11-14 15:09:46 +0100

    FEAT: add textBox to bag

---
+++ src/gui/TextBox.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextBox extends JLabel {
+    public TextBox(String text) {
+        ImageIcon icon = new ImageIcon("../Assets/text_box.png");
+        setOpaque(false);
+        Image textBoxImage = icon.getImage();
+        Image image = textBoxImage.getScaledInstance(610, 96, Image.SCALE_SMOOTH);
+        this.setFont(UIManager.getFont("Label.font").deriveFont(30f));
+        icon.setImage(image);
+        this.setIcon(icon);
+        this.setHorizontalTextPosition(JLabel.CENTER);
+        this.setVerticalTextPosition(JLabel.CENTER);
+        this.setText(text);
+    }
+}
