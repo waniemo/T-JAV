26cc8036ca2c1cda7bff92c2205140eefdf542fc
mminguez <mathieu.minguez@gmail.com>
2023-11-14 13:28:00 +0100

    FEAT: Add change pokemon logic to swing (graphic not yet updated)

---
+++ src/gui/ChangePokemon.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import Team.Team;
+
+public class ChangePokemon extends JPanel {
+    private Image backgroundImage;
+    public ChangePokemon(App frame, ArenaPanel arena, Team playerTeam, Team enemyTeam) {
+        try {
+            backgroundImage = ImageIO.read(new File("../Assets/change_pokemon_bg.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        TeamGrid playerTeamGrid = new TeamGrid(frame, playerTeam, Color.BLUE);
+
+        JLabel playerTitle = new JLabel("CHOISIS UN AUTRE POKEMON");
+
+        JButton backButton = new JButton("RETOUR");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                arena.getPlayerPvBar().updateBar();
+                frame.setLayout(new BorderLayout());
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(arena, BorderLayout.CENTER);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        add(playerTeamGrid, gbc);
+        gbc.gridy = 1;
+        gbc.insets = new Insets(20, 20, 20, 20);
+        add(backButton, gbc);
+        gbc.gridy = 2;
+        add(playerTitle, gbc);
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image scaledBackground = backgroundImage.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+        g.drawImage(scaledBackground, 0, 0, this);
+    }
+}
