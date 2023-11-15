cd7bd89662c6307e1e151e055635a83217baaeb9
waniemo <evaducere@gmail.com>
2023-11-15 10:18:57 +0100

    FEAT : Display attacks when button attaquer is clicked

--- src/gui/PanelHelper.java
+++ src/gui/PanelHelper.java
@@ -1,11 +1,13 @@
 package gui;
 
 import Team.*;
+import Type.*;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class PanelHelper {
     public static JPanel createTeamPanel(App frame, Team team) {
@@ -80,6 +82,51 @@ public class PanelHelper {
                 buttonPanel.removeAll();
                 buttonPanel.revalidate();
                 buttonPanel.repaint();
+                ImageIcon attaque1 = new ImageIcon("../Assets/normal_attaque.png");
+                ImageIcon attaque2 = null;
+                ImageIcon attaque3 = null;
+                ImageIcon attaque4 = null;
+                try {
+                    System.out.println(playerTeam.getActivePokemon().getType().getDeclaredConstructor().newInstance().getName().toLowerCase());
+                    attaque2 = new ImageIcon("../Assets/"+playerTeam.getActivePokemon().getType().getDeclaredConstructor().newInstance().getName().toLowerCase()+"_attaque.png");
+                    attaque3 = new ImageIcon("../Assets/"+playerTeam.getActivePokemon().getType().getDeclaredConstructor().newInstance().getName().toLowerCase()+"_attaque.png");
+                    attaque4 = new ImageIcon("../Assets/"+playerTeam.getActivePokemon().getType().getDeclaredConstructor().newInstance().getName().toLowerCase()+"_attaque.png");
+                } catch (Exception exception) {
+                }
+                ArrayList<JLabel> attaqueLabels = new ArrayList<>();
+                JLabel attaqueLabel1 = new JLabel(attaque1);
+                JLabel attaqueLabel2 = new JLabel(attaque2);
+                JLabel attaqueLabel3 = new JLabel(attaque3);
+                JLabel attaqueLabel4 = new JLabel(attaque4);
+                attaqueLabels.add(attaqueLabel1);
+                attaqueLabels.add(attaqueLabel2);
+                attaqueLabels.add(attaqueLabel3);
+                attaqueLabels.add(attaqueLabel4);
+
+                for(int i = 0; i<4; i++){
+                    String PP = playerTeam.getActivePokemon().getAttaques().get(i).getName() + " "+Integer.toString(playerTeam.getActivePokemon().getAttaques().get(i).getPp())+ "/"+ Integer.toString(playerTeam.getActivePokemon().getAttaques().get(i).getPpMax());
+                    attaqueLabels.get(i).setText(PP);
+                    attaqueLabels.get(i).setHorizontalTextPosition(JLabel.CENTER);
+                    attaqueLabels.get(i).setVerticalTextPosition(JLabel.CENTER);
+                    attaqueLabels.get(i).setForeground(Color.white);
+                    attaqueLabels.get(i).setFont(UIManager.getFont("Label.font").deriveFont(26f));
+                }
+
+                buttonGbc.gridx = 0;
+                buttonGbc.gridy = 0;
+                buttonPanel.add(attaqueLabel1, buttonGbc);
+
+                buttonGbc.gridx = 1;
+                buttonGbc.gridy = 1;
+                buttonPanel.add(attaqueLabel2, buttonGbc);
+
+                buttonGbc.gridx = 1;
+                buttonGbc.gridy = 0;
+                buttonPanel.add(attaqueLabel3, buttonGbc);
+
+                buttonGbc.gridx = 0;
+                buttonGbc.gridy = 1;
+                buttonPanel.add(attaqueLabel4, buttonGbc);
             }
         });
         return buttonPanel;
